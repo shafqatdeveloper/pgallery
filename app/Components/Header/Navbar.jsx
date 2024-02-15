7b2407ee967baa5760a11b5d1a52ea1951aefe8f
shafqatdeveloper <mshafqat.developer@gmail.com>
2024-02-15 17:14:21 +0500

    Mobile Navbar

--- app/Components/Header/Navbar.jsx
+++ app/Components/Header/Navbar.jsx
@@ -74,7 +74,7 @@ useEffect(() => {
 });
   const [dropDownOpened, setdropDownOpened] = useState(false);
   return (
-    <div className={isScrolling ? "w-full fixed z-10 bg-white text-black top-0 left-0" : "w-full fixed text-white z-10 top-0 left-0"}>
+    <div className={isScrolling ? "w-full fixed z-20 bg-white text-black top-0 left-0" : "w-full fixed text-white z-20 top-0 left-0"}>
       <div className="w-full z-10 py-2 gap-5 flex items-center justify-between">
         <div className="pl-2 w-full flex items-center gap-2.5 md:w-min md:pl-8">
           <h1>Pixabay</h1>
@@ -244,7 +244,7 @@ useEffect(() => {
 
       </div>
         {/* Mobile Navbar Code Starts */}
-        <div className={isNavOpen ? "w-full h-screen text-black bg-white fixed top-0 left-0 transition-all duration-700" : "w-full transition-all duration-700 h-screen top-[-100%] bg-white fixed"}>
+        <div className={isNavOpen ? "w-full z-30 text-black bg-white fixed top-0 left-0 transition-all duration-700" : "w-full transition-all duration-700 z-30 top-[-100%] bg-white fixed"}>
         <div className="pl-2 pt-2 md:pl-8">
           <h1 className="text-xl font-bold">Pixabay</h1>
         </div>

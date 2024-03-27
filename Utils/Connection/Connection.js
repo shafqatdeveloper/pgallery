2a03009e49bcc7d921f92bf8af0fbc5c667c0e44
mshafqat.developer <mshafqat.developer@gmail.com>
2024-03-27 12:41:21 +0000

    pic

--- Utils/Connection/Connection.js
+++ Utils/Connection/Connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectToDb = async () => {
   try {
     mongoose.connect(
-      "mongodb+srv://msr:Mshafqat@cluster0.ovk8fn9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://mshafqat3436:Msr102030@cluster0.dwnhd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
     console.log("Connected to Database");
   } catch (error) {
--- app/api/user/register/route.js
+++ app/api/user/register/route.js
@@ -20,27 +20,46 @@ export async function POST(Request) {
     const headerList = headers();
     const requestOrigin = headerList.get("origin");
     const checkUser = await User.findOne({ username });
+    // await User.create({
+    //       name,
+    //       email,
+    //       username,
+    //       password,
+    //     });
+    //          return NextResponse.json("User Registered", {
+    //     status: 201,
+    //     statusText: "Success",
+    //   });
     if (checkUser) {
       return NextResponse.json("Username already exists", {
         status: 401,
         statusText: "Forbidden",
       });
     } else {
-      const newName = username + "profilepic" + path.extname(profilePic.name);
-      const profile = new File([profilePic], newName, {
-        type: profilePic.type,
-        lastModified: profilePic.lastModified,
-      });
-      const filePath = `${requestOrigin}/public/uploads/${profile.name}`;
-      await pump(profile.stream(), fs.createWriteStream(filePath));
-      const profileUrl = `${requestOrigin}/public/uploads/${profile.name}`;
-      await User.create({
-        name,
-        email,
-        username,
-        password,
-        profile: profileUrl,
-      });
+      if(profilePic === null){
+        await User.create({
+          name,
+          email,
+          username,
+          password,
+        });
+      }else{
+        const newName = username + "profilepic" + path.extname(profilePic.name);
+        const profile = new File([profilePic], newName, {
+          type: profilePic.type,
+          lastModified: profilePic.lastModified,
+        });
+        const filePath = `./public/uploads/${profile.name}`;
+        await pump(profile.stream(), fs.createWriteStream(filePath));
+        const profileUrl = `${requestOrigin}/public/uploads/${profile.name}`;
+        await User.create({
+          name,
+          email,
+          username,
+          password,
+          profile: profileUrl,
+        });
+      }
       return NextResponse.json("User Registered", {
         status: 201,
         statusText: "Success",

a21f3994fbd8c5f5ceba5f433dd5a4c30b57f691
shafqatdeveloper <mshafqat.developer@gmail.com>
2024-03-27 12:19:25 +0500

    test

--- Utils/Connection/Connection.js
+++ Utils/Connection/Connection.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDb = async () => {
   try {
-    mongoose.connect(
-      "mongodb+srv://msr:Mshafqat@cluster0.ovk8fn9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {}
-    );
+    mongoose.connect("mongodb://0.0.0.0:27017/pgallery");
     console.log("Connected to Database");
   } catch (error) {
     console.log("Error Connecting to DB ", error.message);
